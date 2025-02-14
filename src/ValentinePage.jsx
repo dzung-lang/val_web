6fece0f864bcbef123863be54fadc7fb968922c9
dzungnguyen21 <83831171+dzung-lang@users.noreply.github.com>
2025-02-14 23:32:37 +0700

    7 fix

--- src/ValentinePage.jsx
+++ src/ValentinePage.jsx
@@ -109,7 +109,7 @@ function ValentinePage() {
       </button>
       {showSchedule && (
         <img
-          src="/val_web/valentine_schedule.png"
+          src="valentine_schedule.png"
           alt="Valentine Schedule"
           style={{
             marginTop: "1rem",
